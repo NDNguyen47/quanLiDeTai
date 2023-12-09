e3f05f55ca7f6d099b398f1022b6a465d6d5e13f
signum6561 <quiocdat777@gmail.com>
2023-12-09 13:50:18 +0700

    fix

--- src/utils/TableUtil.java
+++ src/utils/TableUtil.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import javafx.application.Platform;
 import javafx.scene.control.TableView;
 import javafx.scene.text.Text;
 
@@ -7,23 +10,24 @@ public class TableUtil
 {
     public static void autoResizeColumns(TableView<?> table)
     {
-        table.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
-        table.getColumns().stream().forEach((column) -> {
-            Text t = new Text(column.getText());
-            double max = t.getLayoutBounds().getWidth();
-            for(int i = 0; i < table.getItems().size(); i++)
-            {
-                if(column.getCellData(i) != null)
-                {
-                    t = new Text(column.getCellData(i).toString());
-                    double calcWidth = t.getLayoutBounds().getWidth();
-                    if(calcWidth > max)
-                    {
-                        max = calcWidth;
-                    }
-                }
-            }
-            column.setPrefWidth(max + 50.0d);
+        table.setColumnResizePolicy((param) -> true );
+        Platform.runLater(() -> customResize(table));
+
+        
+    }
+
+    public static void customResize(TableView<?> view) {
+
+        AtomicLong width = new AtomicLong();
+        view.getColumns().forEach(col -> {
+            width.addAndGet((long) col.getWidth());
         });
+        double tableWidth = view.getWidth();
+
+        if (tableWidth > width.get()) {
+            view.getColumns().forEach(col -> {
+                col.setPrefWidth(col.getWidth()+((tableWidth-width.get())/view.getColumns().size()));
+            });
+        }
     }
 }
